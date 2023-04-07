7ffa2d16f45a429808e62fcb4ceb4b2fd7bf52fa
AlexiosVavvas <alexios.vavvas@gmail.com>
2023-04-07 13:26:09 +0300

    Minor changes + Some cleanup to be more readable 	- changed names (eg: offset -> pitot_offset) 	- Sd card pin absent at SD.begin() 	- offset_size -> offset_times (name not defined) 	- myFile object not reOpened for writing after each loop Questions: 	- small delay at pitot offset calculation? 	- airspeed in [m/s]? 	- 10sec delay for stability??

--- efficiencyMap.ino
+++ efficiencyMap.ino
@@ -2,67 +2,81 @@
 #include <SPI.h>
 #include <SD.h>
 
-
 HX711 scale;
+File myFile; 
 
-float airspeed = 0;
-float d = 1.204; // air density (kg/m3)
-
-int offset = 0; // offset if there is flow when starting pitot
-int offset_times = 10; 
+// Input Variables --------------
 
 const int SDpin = 10; // SD CS pin 
-File myFile; // we create a file to store the data
+const int LOADCELL_DOUT_PIN = 2;
+const int LOADCELL_SCK_PIN = 13;
+const int PITOT_PIN = A0;
+
+float air_density = 1.204; // air density (kg/m3)
+float loadcell_scale = 2000.0;
+
+char filename[] = "Measurements.csv";
+
+// ------------------------------
+
+float airspeed = 0;
+int pitot_offset = 0; // offset if there is flow when starting pitot
+int pitot_offset_times = 10; 
+
 
 void setup(){
   Serial.begin(38400); // too high for pitot maybe??
-  scale.begin(2,13); // hx711 pins 
 
-// initialise SD card
+  // Initialise SD card
   Serial.print("Initializing SD card...");
-
   if (!SD.begin()) {
     Serial.println("initialization failed!");
     return;
   }
   Serial.println("initialization done.");
 
-  myFile = SD.open("test.csv", FILE_WRITE); // opens or creates SD file 
+  myFile = SD.open(filename, FILE_APPEND); // opens or creates SD file 
+  myFile.println("--------\n")
+  myFile.close()
 
-
-  scale.set_scale(2000.0); // adding calibration factor to dyno (reading/known weight) (not determined yet)
+  //Setting up HX711
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN); // hx711 pins 
+  scale.set_scale(loadcell_scale); // adding calibration factor to dyno (reading/known weight) (not determined yet)
   scale.tare(); // resets to zero
 
-// we test for offset 10 times and we take the average offset
-
-  for(int i=0; i<offset_times; i++){
-    offset += analogRead(A0) - (1023/2);
+  //Setting up PITOT
+  // we test for offset 10 times and we take the average offset
+  for(int i=0; i<pitot_offset_times; i++){
+    pitot_offset += analogRead(PITOT_PIN) - (1023/2);
   }
-  offset /= offset_size;
+  pitot_offset /= pitot_offset_times;
+
 }
 
 
 void loop(){
-  float v_read = analogRead(A0)-offset; //pitot voltage read minus offset
 
-// if it reads below 512 we need to equate to a negative velocity
+  // Reading pitot
+  float v_read = analogRead(PITOT_PIN)-pitot_offset; 
 
+  // if it reads below 512 we need to equate to a negative velocity
   if(v_read<512){
-    airspeed = -sqrt((-10000.0*((v_read/1023.0)-0.5))/d);
+    airspeed = -sqrt((-10000.0*((v_read/1023.0)-0.5))/air_density);
   }else{
-    airspeed = sqrt((-10000.0*((v_read/1023.0)-0.5))/d);
+    airspeed = sqrt((-10000.0*((v_read/1023.0)-0.5))/air_density);
   }
   
-
   // pitot should delay 10secs for stability
+  Serla.print("/*") //Format for Serial Studio
   Serial.print(scale.get_units(),3); // reading minus tare and divided by calibration parameter
-  Serial.print("\t");
+  Serial.print(",");
   Serial.print(airspeed);
-  Serial.print("\n")
+  Serial.print("*/\n")
 
-// store data in SD card
-// firt we check that the file is open and working
-   if (myFile) {
+  // Store data in SD card
+  // firt we check that the file is open and working
+  myFile = SD.open(filename, FILE_APPEND);
+  if (myFile) {
     myFile.println(scale.get_units(),3);
     myFile.print("\t");
     myFile.print(airspeed);
@@ -70,6 +84,6 @@ void loop(){
     myFile.close(); // ensures the data is saved
     
   } else {
-    Serial.println("error opening test.txt");
+    Serial.println("Error opening file for writing!");
   }
 }
