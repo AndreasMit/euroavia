9924e5ec1c28876d212e40ea886d0523a651333b
AlexiosVavvas <alexios.vavvas@gmail.com>
2023-09-04 20:28:50 +0300

    WindTunnel Update 2 	- Fixed Pitot 	- Add rpm measurement with interrupts 	- Cleaner setup and loop (added assisting functions)

--- efficiency map/efficiencyMap_NO_NRF/efficiencyMap_NO_NRF.ino
+++ efficiency map/efficiencyMap_NO_NRF/efficiencyMap_NO_NRF.ino
@@ -14,7 +14,8 @@ const byte LOADCELL_SCK_PIN = A4;
 const byte PITOT_PIN = A7;
 const byte motor_pin = A3;
 
-// double air_density = 1.204; // air density (kg/m3)
+const byte RPM_PIN = 2;
+
 const byte pitot_offset_times = 10; // how many times to test for offset at the beginning of the code
 const byte pitot_readings_per_loop = 10;
 double loadcell_scale = 2000.0;
@@ -26,15 +27,24 @@ double loadcell_scale = 2000.0;
 //Variables Needed
 double pitot_offset = 0;  // offset if there is flow when starting pitot
 double pitot_airspeed = 0;
+double pitot_pressure = 0;
+double pitot_adc_raw = 0;
+double pitot_voltage = 0;
+double air_density = 1;
+
 int file_offset = 0;  // This is the file offset that will be appended to the filename.
 
 double dyn_force = 0;
 
-String date_time = "";
+String comments = "";
 unsigned long currentMillis = millis();
+unsigned long int prev_time_interups;
 unsigned long prev_time = 0;
 float wt_loop_freq = 0;  //wind tunnel arduino loop freq
 int motor_speed = 1000;
+
+int rpm = 0;
+volatile float objects = 0;
 
 
 //Commands
@@ -51,9 +61,18 @@ void setup() {
   motor.writeMicroseconds(1000);
   delay(500);
 
+  // RPM Config
+  attachInterrupt(digitalPinToInterrupt(2), count_rpm, FALLING);
+  delay(1000);
+  pinMode(RPM_PIN, INPUT);
+
   // Getting time and date from user
-  Serial.println("Enter time and date in the following format: 2023-08-30 14:25:45");
-  readDateTimeFromSerial();
+  Serial.println("Enter any comments regarding the setup of the experiment: ");
+  readCommentsTimeFromSerial();
+
+
+  //Get air density from user
+  readDensityFromSerial();
   
   
   // Scale Init
@@ -70,43 +89,16 @@ void setup() {
   pitot_offset /= pitot_offset_times;
 
 
-  // SD CARD BEGIN INITIALIZATION ----------------
-  Serial.println("Should I initialize the SD Card?");  // Once 'y' is typed in the Serial monitor the program should go on...
-  
-  Serial.flush();
-  while (serial_command != 'y') {
-    while (Serial.available() == 0) {
-      // Wait for user input
-    }
-    
-    serial_command = Serial.read();
-    Serial.flush();
-  }
-  serial_command = 'n';
-  delay(1000);
+  // SD CARD INIT
+  // sdCardInit();
 
-  Serial.println("Initializing SD card..."); //send message back too
-  
-  delay(200);
-
-  if (!SD.begin()) {
-    Serial.println("Initialization failed!"); //send message back too
-    delay(1000);
-    // return;
-  }
-  Serial.println("Initialization done."); //send message back
-  delay(200);
-
-  // sdOpenFile();
-  // SD CARD END INITIALIZATION
 
   //Asking user for motor speed input
   askForMotorSpeed();
   
   //Printing Status
-  Serial.println("Format: Time[ms] / MotorSpeed [1000 - 2000] / dyn_force / pitot_airspeed");
-
-  Serial.println("Entering Main Loop");
+  Serial.println("Format: ");
+  Serial.println("Time[ms] / Loop Freq [Hz] / MotorSpeed [1000 - 2000] / Dyn_force [g] / RPM / Pitot_adc / Pitot_voltage / Pitot_pressure [Pa] / Pitot_speed [m/s]");
 
 }
 
@@ -136,13 +128,15 @@ void loop(){
 	for (int i = 0; i < pitot_readings_per_loop; i++){
 		v_read += analogRead(PITOT_PIN) - pitot_offset;
 	}
-	pitot_airspeed = v_read/pitot_readings_per_loop;
+	pitot_adc_raw = v_read/pitot_readings_per_loop;
+	pitot_voltage = pitot_adc_raw / 1023 * 5;
+	pitot_pressure = 1000 * (pitot_voltage - 2.5);  // [Pa]
 
-	// if it reads below 512 we need to equate to a negative velocity
-	// if (v_read < 512)
-	//   data.pitot_airspeed = -sqrt((-10000.0*((v_read / 1023.0) - 0.5)) / air_density); //maybe we could simplify these operations
-	// else
-	//   data.pitot_airspeed = +sqrt((+10000.0*((v_read / 1023.0) - 0.5)) / air_density);
+	// if it reads above 512 we need to equate to a negative velocity
+	if (v_read < 512)
+	  pitot_airspeed = +sqrt(2 * abs(pitot_pressure) / air_density); //maybe we could simplify these operations
+	else
+	  pitot_airspeed = -sqrt(2 * abs(pitot_pressure) / air_density);
 
 
 	// Reading Scale
@@ -154,6 +148,15 @@ void loop(){
 	//Storing Freq
 	wt_loop_freq = (float)(1000/(millis() - prev_time)); // Frequency [Hz]
 
+	//RPM Measurement
+	delay(500);
+  detachInterrupt(digitalPinToInterrupt(2));
+  rpm = objects/(millis() - prev_time) * 60000;
+	prev_time_interups = millis();
+  objects = 0;
+  attachInterrupt(digitalPinToInterrupt(2), count_rpm, FALLING);
+
+
 	// Sending to Serial Studio
 	Serial.print("/*"); // Format for Serial Studio
 	Serial.print(millis());
@@ -163,6 +166,14 @@ void loop(){
 	Serial.print(motor_speed);
 	Serial.print(",");
 	Serial.print(dyn_force); // Reading minus tare and divided by calibration parameter
+	Serial.print(",");
+	Serial.print(rpm);
+	Serial.print(",");
+	Serial.print(pitot_adc_raw);
+	Serial.print(",");
+	Serial.print(pitot_voltage);
+	Serial.print(",");
+	Serial.print(pitot_pressure);
 	Serial.print(",");
 	Serial.print(pitot_airspeed);
 	Serial.print("*/\n");
@@ -232,17 +243,39 @@ void sdOpenFile(){
     Serial.println("Something went wrong while trying to open the file" + get_filename(file_offset));
 }
 
-void readDateTimeFromSerial(){
+void readCommentsTimeFromSerial(){
 	Serial.flush();
 	while (Serial.available() == 0) {
 		// Wait for user input
 	}
-	date_time = Serial.readString();
+	comments = Serial.readString();
 	Serial.flush();
 	Serial.println("---------------------");
-	Serial.println(date_time);
+	Serial.println(comments);
 	Serial.println("---------------------");
 	delay(1000);
+}
+
+void readDensityFromSerial(){
+	float input = 0;
+
+	Serial.println("Choose AIR DENSITY for the experiment:");
+	Serial.println("http://meteo.ntua.gr/");
+	Serial.println("https://www.calctool.org/atmospheric-thermodynamics/air-density");
+	Serial.flush();
+	while (Serial.available() == 0) {
+		// Wait for user input
+	}
+	input = Serial.parseFloat();
+	input = (double)(input);
+	Serial.flush();
+	Serial.println("---------------------");
+	Serial.print("Air Density: "); Serial.println(input, 6);
+	Serial.println("---------------------");
+	delay(1000);
+
+	air_density = input;
+
 }
 
 void askForMotorSpeed(){
@@ -261,4 +294,44 @@ void askForMotorSpeed(){
 		delay(1000);
 	}
 	motor_speed = input;
+}
+
+void sdCardInit(){
+
+	// SD CARD BEGIN INITIALIZATION ----------------
+  Serial.println("Should I initialize the SD Card?");  // Once 'y' is typed in the Serial monitor the program should go on...
+  
+  Serial.flush();
+  while (serial_command != 'y') {
+    while (Serial.available() == 0) {
+      // Wait for user input
+    }
+    
+    serial_command = Serial.read();
+    Serial.flush();
+  }
+  serial_command = 'n';
+  delay(1000);
+
+  Serial.println("Initializing SD card..."); //send message back too
+  
+  delay(200);
+
+  if (!SD.begin()) {
+    Serial.println("Initialization failed!"); //send message back too
+    delay(1000);
+    // return;
+  }
+  else {
+  	Serial.println("Initialization done."); //send message back
+  }
+  delay(200);
+
+  // sdOpenFile();
+  // SD CARD END INITIALIZATION
+
+}
+
+void count_rpm() {
+  ++objects;
 }
