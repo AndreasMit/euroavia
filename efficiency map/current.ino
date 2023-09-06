aadb8959fad4c4465be14165372b3b6ec8a88241
andmit <andmit@rico.lan>
2023-09-06 11:36:08 +0300

    current measurement

--- efficiency map/current.ino
+++ efficiency map/current.ino
@@ -1,30 +1,60 @@
-#include <Wire.h>
-#include <Adafruit_ADS1015.h>
-Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
+#include <Adafruit_ADS1X15.h>
+
+//Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
+Adafruit_ADS1015 ads;     /* Use this for the 12-bit version */
 
 void setup(void)
 {
-Serial.begin(9600);
+  Serial.begin(9600);
+  Serial.println("Hello!");
 
-ads.setGain(GAIN_SIXTEEN);    // 16x gain +/- 0.256V 1 bit = 0.125mV 0.0078125mV
+  Serial.println("Getting single-ended readings from AIN0..3");
+  Serial.println("ADC Range: +/- 6.144V (1 bit = 3mV/ADS1015, 0.1875mV/ADS1115)");
 
-ads.begin();
+  // The ADC input range (or gain) can be changed via the following
+  // functions, but be careful never to exceed VDD +0.3V max, or to
+  // exceed the upper and lower limits if you adjust the input range!
+  // Setting these values incorrectly may destroy your ADC!
+  //                                                                ADS1015  ADS1115
+  //                                                                -------  -------
+  // ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
+  // ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
+  // ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
+  // ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
+  // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
+  ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
+
+  if (!ads.begin()) {
+    Serial.println("Failed to initialize ADS.");
+    while (1);
+  }
 }
 
 void loop(void)
 {
-int16_t results;
+  int16_t adc0, adc1, adc2, adc3;
+  double volts0, volts1, volts2, volts3, current;
 
-results = ads.readADC_Differential_0_1();
+  // adc0 = ads.readADC_SingleEnded(0);
+  adc1 = ads.readADC_SingleEnded(1);
+  // adc2 = ads.readADC_SingleEnded(2);
+  // adc3 = ads.readADC_SingleEnded(3);
 
-Serial.print(“Amps: “);
+  // volts0 = ads.computeVolts(adc0);
+  // counts = adc1;
+  // fsRange = 0.256f;
+  volts1 = adc1 * (0.256f / (32768 >> 0));
+  // current = volts1/0.00185;
+  current = volts1 * 50*16/0.075;
+  Serial.println(current);
+  // volts2 = ads.computeVolts(adc2);
+  // volts3 = ads.computeVolts(adc3);
 
-float amps = ((float)results * 256.0) / 32768.0;//100mv shunt
-//amps = amps * 1.333; //uncomment for 75mv shunt
-//amps = amps * 2; //uncomment for 50mv shunt
+  // Serial.println("-----------------------------------------------------------");
+  // // Serial.print("AIN0: "); Serial.print(adc0); Serial.print("  "); Serial.print(volts0); Serial.println("V");
+  // Serial.print("AIN1: "); Serial.print(adc1); Serial.print("  "); Serial.print(volts1); Serial.println("V");
+  // Serial.print("AIN2: "); Serial.print(adc2); Serial.print("  "); Serial.print(volts2); Serial.println("V");
+  // Serial.print("AIN3: "); Serial.print(adc3); Serial.print("  "); Serial.print(volts3); Serial.println("V");
 
-Serial.println(amps);
-
-delay(5000);
+  // delay(1000);
 }
-
