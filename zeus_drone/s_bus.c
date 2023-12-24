df0ebd41ae4c872f848fc8caefbe2ff5853a58df
KostasPik <kostas.pikoulas2002@gmail.com>
2023-12-24 17:14:52 +0200

    zeus sbus

--- zeus_drone/s_bus.c
+++ zeus_drone/s_bus.c
@@ -3,13 +3,14 @@
 #include "stdlib.h"
 #include "string.h"
 #include "fcntl.h"
+#include <sys/types.h>
 #include "unistd.h"
 #include "termios.h"
 
 
 
 
-int sbus_write(int sbusFile, const struct SBUSFrame *msg) {
+uint8_t sbus_write(int sbusFile, const struct SBUSFrame *msg) {
 
 
     if (!sbusFile || !msg) {
@@ -20,21 +21,56 @@ int sbus_write(int sbusFile, const struct SBUSFrame *msg) {
 
     uint8_t buf[SBUS_FRAME_LENGTH];
     memset(buf, 0, sizeof(buf));
+
+
     // Header Frame
     buf[0] = msg->startByte;
+
+    // Channels
+    buf[1] = (uint8_t)((msg->channels[0] & 0x07FF));
+    buf[2] = (uint8_t)((msg->channels[0] & 0x07FF) >> 8 |
+                        (msg->channels[1] & 0x07FF) << 3);
+    buf[3] = (uint8_t)((msg->channels[1] & 0x07FF) >> 5 |
+                        (msg->channels[2] & 0x07FF) << 6);
+    buf[4] = (uint8_t)((msg->channels[2] & 0x07FF) >> 2);
+    buf[5] = (uint8_t)((msg->channels[2] & 0x07FF) >> 10 |
+                        (msg->channels[3] & 0x07FF) << 1);
+    buf[6] = (uint8_t)((msg->channels[3] & 0x07FF) >> 7 |
+                        (msg->channels[4] & 0x07FF) << 4);
+    buf[7] = (uint8_t)((msg->channels[4] & 0x07FF) >> 4 |
+                        (msg->channels[5] & 0x07FF) << 7);
+    buf[8] = (uint8_t)((msg->channels[5] & 0x07FF) >> 1);
+    buf[9] = (uint8_t)((msg->channels[5] & 0x07FF) >> 9 |
+                        (msg->channels[6] & 0x07FF) << 2);
+    buf[10] = (uint8_t)((msg->channels[6] & 0x07FF) >> 6 |
+                        (msg->channels[7] & 0x07FF) << 5);
+    buf[11] = (uint8_t)((msg->channels[7] & 0x07FF) >> 3);
+    buf[12] = (uint8_t)((msg->channels[8] & 0x07FF));
+    buf[13] = (uint8_t)((msg->channels[8] & 0x07FF) >> 8 |
+                        (msg->channels[9] & 0x07FF) << 3);
+    buf[14] = (uint8_t)((msg->channels[9] & 0x07FF) >> 5 |
+                        (msg->channels[10] & 0x07FF) << 6);
+    buf[15] = (uint8_t)((msg->channels[10] & 0x07FF) >> 2);
+    buf[16] = (uint8_t)((msg->channels[10] & 0x07FF) >> 10 |
+                        (msg->channels[11] & 0x07FF) << 1);
+    buf[17] = (uint8_t)((msg->channels[11] & 0x07FF) >> 7 |
+                        (msg->channels[12] & 0x07FF) << 4);
+    buf[18] = (uint8_t)((msg->channels[12] & 0x07FF) >> 4 |
+                        (msg->channels[13] & 0x07FF) << 7);
+    buf[19] = (uint8_t)((msg->channels[13] & 0x07FF) >> 1);
+    buf[20] = (uint8_t)((msg->channels[13] & 0x07FF) >> 9 |
+                        (msg->channels[14] & 0x07FF) << 2);
+    buf[21] = (uint8_t)((msg->channels[14] & 0x07FF) >> 6 |
+                        (msg->channels[15] & 0x07FF) << 5);
+    buf[22] = (uint8_t)((msg->channels[15] & 0x07FF) >> 3);
+
+    // Flag Byte
+    buf[23] = msg->flagByte;
+
     // End Frame
     buf[SBUS_FRAME_LENGTH - 1] = msg->endByte;
 
-    uint8_t buffer_counter = 1;
-    for( uint8_t channel_counter = 1; channel_counter < SBUS_NUM_CHANNELS; ++channel_counter ) {
-        if (channel_counter == 1) {
-            buf[buffer_counter++] = msg->channels[channel_counter] & 0xFF;
-            continue;
-        }
-        buf[buffer_counter++] = ((msg->channels[channel_counter - 1] >> 8) & 0b111) |  ((msg->channels[channel_counter] & 0b11111) << 3);   
-    }
 
-    // DO SOMETHING WITH FLAG byte
     ssize_t bytes_written = write(sbusFile, buf, sizeof(buf));
     
     if (bytes_written == -1) {
@@ -81,11 +117,18 @@ int sbus_open() {
 }
 
 
-void initialize_sbus_msg(struct SBUSFrame *msg) {
+void clear_msg_channels(struct SBUSFrame *msg) {
+    memset(msg->channels, 0, sizeof(msg->channels));
+}
 
-    sbusFrame->startByte = 0x0F;
+uint8_t initialize_sbus_msg(struct SBUSFrame *msg) {
+
+    msg->startByte = 0x0F;
 
     memset(msg->channels, 0, sizeof(msg->channels));
+
+    // make flagByte 0x00
+    msg->flagByte = 0x00;
 
     // INIT THE FLAG TO SOMETHING!
     msg->endByte = 0x00;
--- zeus_drone/s_bus.h
+++ zeus_drone/s_bus.h
@@ -2,10 +2,12 @@
 #define SBUS_FRAME_LENGTH 25
 #define SBUS_NUM_CHANNELS 16
 #define SBUS_BAUD_RATE B100000
+// 200000 Baud Rate for "Fast SBUS"
 
-// Success / Error types
-#define SBUS_SUCCESS = 5
-#define SBUS_ERROR = -1;
+
+// Success / Error codes
+#define SBUS_SUCCESS 5
+#define SBUS_ERROR -1;
 
 
 // 16 bit channels. Only 11 bits will be used per channel!
@@ -13,6 +15,6 @@
 struct SBUSFrame {
     uint8_t startByte;
     uint16_t channels[SBUS_NUM_CHANNELS];
-    uint8_t flag;
+    uint8_t flagByte;
     uint8_t endByte;
 };
